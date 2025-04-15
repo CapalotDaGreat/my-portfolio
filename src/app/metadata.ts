3a03a456a4fd867b08b82c94a125b042027451ef
CML <lusiolac@bzz.ch>
2025-04-15 09:11:07 +0200

    Porto

---
+++ src/app/metadata.ts
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: {
+        default: "Mein Portfolio",
+        template: "%s | Mein Portfolio"
+    },
+    description: "Portfolio mit Next.js",
+    keywords: ["Portfolio", "Web Development", "Next.js", "React"],
+};
